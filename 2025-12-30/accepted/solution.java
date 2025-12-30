3d45f1a0a8101b8a9152a8ac2ade881863d96a59
Isac Pederson <isacjped@gmail.com>
2025-12-29 17:42:32 -0800

    Added accepted on 2025-12-30

--- 2025-12-30/accepted/solution.java
+++ 2025-12-30/accepted/solution.java
@@ -1,51 +1,23 @@
 class Solution {
-    public String minWindow(String s, String t) {
-        if(t.length() > s.length()){
-            return "";
-        }
-        Map<Character, Integer> freq = new HashMap<>();
-        Map<Character, Integer> window = new HashMap<>();
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a,b) -> b[0] - a[0]);
+        int[] res = new int[nums.length-k+1];
+        int index = 0;
 
 
-        for(char c : t.toCharArray()){
-            freq.put(c,freq.getOrDefault(c,0)+1);
-        }
-
-        int l = 0;
-        int have = 0;
-        int need = freq.size();
-        int[] res = {-1,-1};
-        int resLen = Integer.MAX_VALUE;
-
-        for(int r =0; r < s.length(); r++){
-            char c =  s.charAt(r);
-            window.put(c,window.getOrDefault(c,0)+1);
-            
-            if(freq.containsKey(c) && freq.get(c).equals(window.get(c))){
-                have++;
-            }
-            
-            while(have == need){
-                if((r-l+1) < resLen){
-                    resLen = r-l+1;
-                    res[0] = l; 
-                    res[1] = r; 
+        for(int i = 0; i < nums.length; i++){
+            heap.offer(new int[]{nums[i], i});
+            if(i >= k-1){
+                while(heap.peek()[1] <= i-k){
+                    heap.poll();
                 }
-
-                char lc = s.charAt(l);
-                window.put(lc,window.get(lc)-1);
-                if(freq.containsKey(lc) && freq.get(lc) > window.get(lc)){
-                    have--;
-                }
-                l++;
+                res[index++] = heap.peek()[0];
             }
+        }
+
+        return res;
 
 
-        } 
-
-        return resLen == Integer.MAX_VALUE ? "" : s.substring(res[0],res[1]+1);
-
-
-
+        
     }
 }
