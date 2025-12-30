1a6cfeb9f7e474ec5635138c70b6c22184951d71
Isac Pederson <isacjped@gmail.com>
2025-12-29 17:43:06 -0800

    Added accepted on 2025-12-30

--- 2025-12-30/accepted/solution.java
+++ 2025-12-30/accepted/solution.java
@@ -1,41 +1,34 @@
-class MinStack {
-    Stack<Integer> stack;
-    Stack<Integer> minStack;
-
-    public MinStack() {
-        stack = new Stack<>();
-        minStack = new Stack<>();
-    }
-    
-    public void push(int val) {
-        if(stack.empty()){
-            minStack.push(val); 
-        }else{
-            if(val <= minStack.peek()){
-                minStack.push(val);
+class Solution {
+    public int evalRPN(String[] tokens) { 
+        Stack<Integer> stack = new Stack<>();
+        for(String token : tokens){
+            if(token.equals("+")){
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(a+b);
             }
+            else if(token.equals("-")){
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b-a);
+            }
+            else if(token.equals("/")){
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b/a);
+            }
+            else if(token.equals("*")){
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(a*b);
+            }else{
+                stack.push(Integer.parseInt(token));
+            }
+
+
         }
-        stack.push(val);
-        
-    }
-    
-    public void pop() {
-        if(stack.isEmpty()) return;
-        int top = stack.pop();
-        if(top == minStack.peek()){
-            minStack.pop();
-        }
-    }
-    
-    public int top() {
+
         return stack.peek();
-    }
-    
-    public int getMin() {
-        if(minStack.empty()){
-            return 0;
-        }else{
-            return minStack.peek();
-        }
+        
     }
 }
