5796960cf4ac869ef86fc9d9e50549162c637b78
Isac Pederson <isacjped@gmail.com>
2025-12-29 17:44:50 -0800

    Added accepted on 2025-12-30

--- 2025-12-30/accepted/solution.java
+++ 2025-12-30/accepted/solution.java
@@ -1,55 +1,35 @@
-class TimeMap {
-    Map<String, List<Pair<Integer,String>>> m;
-
-    public TimeMap() {
-        m = new HashMap<>();
-    }
-    
-    public void set(String key, String value, int timestamp) {
-        m.computeIfAbsent(key, k -> new ArrayList<>()).add(new Pair<>(timestamp, value));
-    }
-    
-    public String get(String key, int timestamp) {
-        List<Pair<Integer, String>> values = m.getOrDefault(key, new ArrayList<>());
-        int r = values.size() -1;
-        int l = 0;
-        String res = "";
-
-        while(l<=r){
-            int m = (l + r) / 2;
-            if(values.get(m).getKey() <= timestamp){
-                res = values.get(m).getValue(); 
-                l = m+1;
-            }else{
-                r = m-1;
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+       int left = (nums1.length + nums2.length +1) / 2; //3
+       int right = (nums1.length + nums2.length +2) / 2; //3.5
+       return (getKth(nums1, nums1.length, nums2, nums2.length, left, 0, 0)
+       +
+            getKth(nums1, nums1.length, nums2, nums2.length, right , 0, 0)) / 2.0;
             }
+
+
+    int getKth(int[] a, int aLen, int[] b, int bLen, int k, int aStart, int bStart){
+        if(aLen > bLen){
+            return getKth(b, bLen, a, aLen, k, bStart, aStart);
+        }
+        if(aLen == 0){
+            return b[bStart + k -1];
+        }
+        if(k == 1){
+            return Math.min(a[aStart], b[bStart]);
         }
 
-        return res;
+        int i = Math.min(aLen, k /2);
+        int j = Math.min(bLen, k/2);
+
+        if(a[aStart + i - 1] > b[bStart + j - 1]){
+            return getKth(a, aLen, b, bLen - j, k - j, aStart, bStart +j );
+        }else{
+            return getKth(a, aLen - i, b, bLen, k - i, aStart + i, bStart );
+        }
 
 
-        
+
     }
-
-
-}
-
-class Pair<K,V>{
-    final K k;
-    final V v;
-
-    public Pair(K key, V value){
-        this.k = key;
-        this.v = value;
-    }
-
-    public K getKey(){
-        return k;
-    }
-
-    public V getValue(){
-        return v;
-    }
-
 
 }
