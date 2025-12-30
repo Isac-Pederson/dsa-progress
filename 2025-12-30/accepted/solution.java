422cd478d1f573f4909174ab466a27458449ea61
Isac Pederson <isacjped@gmail.com>
2025-12-29 17:41:07 -0800

    Added accepted on 2025-12-30

--- 2025-12-30/accepted/solution.java
+++ 2025-12-30/accepted/solution.java
@@ -1,23 +1,39 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        int left = 0;
-        int right = numbers.length - 1;
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res =  new ArrayList<>();
 
-        while(left < right){
-            System.out.println(right);
-            int currSum = numbers[left] + numbers[right];
-            if(currSum == target){
-                return new int[]{left+1, right+1};
+
+        for(int i = 0; i < nums.length; i++){
+            int l = i+1;
+            int r = nums.length-1;
+            if(i > 0 && nums[i] == nums[i-1]) continue; 
+
+                //dupe check
+
+            while(l < r){
+
+               int sum  = nums[i] + nums[l] + nums[r];
+
+               if(sum > 0){
+                r--;
+               }else if(sum < 0){
+                l++;
+               }else{
+                //match found
+                res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                l++;
+                r--;
+                //dupe check
+                while(l < r && nums[l] == nums[l-1]){
+                    l++;
+                }
+               }
             }
-            if(currSum < target){
-                left++;
-            }else{
-                right--;
-            }
+
+
         }
 
-
-        return new int[]{};
-        
+        return res;
     }
 }
