2c7bca53433588cd16c34cf0a8d9a0ca30f1c771
Isac Pederson <isacjped@gmail.com>
2025-12-29 17:39:43 -0800

    Added accepted on 2025-12-30

--- 2025-12-30/accepted/solution.java
+++ 2025-12-30/accepted/solution.java
@@ -1,17 +1,25 @@
 class Solution {
-    public boolean hasDuplicate(int[] nums) {
-        Set<Integer> seenNums = new HashSet<>();
-
-        for(int i = 0; i < nums.length; i++){
-            if(seenNums.contains(nums[i])){
-                return true;
-            }
-            seenNums.add(nums[i]);
-
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> res = new HashMap<>();
+        if(s.length() != t.length()){
+            return false;
+        }
+        //create map, char as key, int as value
+        //if key doesnt exist in t return false
+        // otherwise return true at end 
+        for(int i = 0; i < s.length(); i++ ){
+            res.put( s.charAt(i),res.getOrDefault(s.charAt(i), 0) + 1);
         }
 
-        return false;
-
- 
+        for(int i = 0; i < t.length(); i++ ){
+            if(res.get(t.charAt(i)) == null){
+                return false;
+            }
+            if(res.get(t.charAt(i)) == 0){
+                return false;
+            }
+            res.put( t.charAt(i),res.getOrDefault(t.charAt(i), 0) - 1);
+        }
+        return true;
     }
 }
