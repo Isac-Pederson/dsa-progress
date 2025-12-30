f327056fc81da22e2a014754e3fbf2341dd286d4
Isac Pederson <isacjped@gmail.com>
2025-12-29 17:41:54 -0800

    Added accepted on 2025-12-30

--- 2025-12-30/accepted/solution.java
+++ 2025-12-30/accepted/solution.java
@@ -1,19 +1,25 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int b = 0;
-        int s = 1;
-        int profit = 0;
+    public int lengthOfLongestSubstring(String s) {
+        int longestSub = 0;
+        HashSet<Character> seenCharacters = new HashSet<>();
+        int l = 0;
 
-        while(s < prices.length){
-            if(prices[s] > prices[b]){
-                profit = Math.max(profit, prices[s] - prices[b]);
-            }else{
-                b = s;
+        for(int r = 0; r < s.length(); r++){
+            while(seenCharacters.contains(s.charAt(r))){
+                seenCharacters.remove(s.charAt(l));
+                l++;
             }
-            s++;
+
+            longestSub = Math.max(longestSub, r-l+1);
+            seenCharacters.add(s.charAt(r));
         }
 
-        return profit;
 
-    }
+        return longestSub;
+        
+
+
+
+        }
+        
 }
