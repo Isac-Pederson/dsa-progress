c18b601b9070d13821606274178fe7e513bd4b9c
Isac Pederson <isacjped@gmail.com>
2025-12-29 17:44:29 -0800

    Added accepted on 2025-12-30

--- 2025-12-30/accepted/solution.java
+++ 2025-12-30/accepted/solution.java
@@ -1,21 +1,31 @@
 class Solution {
-    public int findMin(int[] nums) {
+    public int search(int[] nums, int target) {
         int l = 0;
         int r = nums.length-1;
 
-        while(l<r){
-            int m = (r+l)/2;
-            if(nums[m] < nums[r]){
-                r = m;
-            }else if(nums[m] > nums[r]){
-                l = m+1;
-            }  
+        while(l<=r){
+            int m = (l+r)/2;
+            if(nums[m] == target){
+                return m;
+            }
+
+            if(nums[m] >= nums[l]){
+                if(nums[l] <= target && target <= nums[m]){
+                    r = m-1;
+                }else{
+                    l=m+1;
+                }
+            }else if(nums[r] >= nums[r]){
+                if(nums[m] <= target && target <= nums[r]){
+                    l = m+1;
+                }else{
+                    r=m-1;
+                }
+            }
 
         }
 
-        return nums[l];
-        
-
+        return -1;
         
     }
 }
