9c63282890bdd8ce0c1066267c500f0a6525079e
Isac Pederson <isacjped@gmail.com>
2025-12-29 17:44:22 -0800

    Added accepted on 2025-12-30

--- 2025-12-30/accepted/solution.java
+++ 2025-12-30/accepted/solution.java
@@ -1,34 +1,20 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
-        int r = 0;
-
-        for(int i = 0; i<piles.length; i++){
-            r = Math.max(r, piles[i]);
-        }
-
+    public int findMin(int[] nums) {
         int l = 0;
-        int res = Integer.MAX_VALUE;
+        int r = nums.length-1;
 
-        while(l<=r){
-            int k = (l+r)/2;
-            double hours = 0;
+        while(l<r){
+            int m = (r+l)/2;
+            if(nums[m] < nums[r]){
+                r = m;
+            }else if(nums[m] > nums[r]){
+                l = m+1;
+            }  
 
-            for(int i = 0; i<piles.length; i++){
-                hours = hours + (double) Math.ceil((double)piles[i] / (double) k);
-            }
-            if(hours > h){
-                l = k+1;
-            }else{
-                r = k-1;
-                res = Math.min(res,k);
-            }
         }
 
-        return res;
-
-
-
-
+        return nums[l];
+        
 
         
     }
