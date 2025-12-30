c78bfd3973b42f9d1c00570f44565fd5a97e72bb
Isac Pederson <isacjped@gmail.com>
2025-12-29 17:41:17 -0800

    Added accepted on 2025-12-30

--- 2025-12-30/accepted/solution.java
+++ 2025-12-30/accepted/solution.java
@@ -1,37 +1,20 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> res =  new ArrayList<>();
+    public int maxArea(int[] heights) {
+        int l = 0;
+        int r = heights.length-1;
+        int res = 0;
 
 
-        for(int i = 0; i < nums.length; i++){
-            int l = i+1;
-            int r = nums.length-1;
-            if(i > 0 && nums[i] == nums[i-1]) continue; 
-
-                //dupe check
-
-            while(l < r){
-
-               int sum  = nums[i] + nums[l] + nums[r];
-
-               if(sum > 0){
-                r--;
-               }else if(sum < 0){
+        while(l < r){
+            int length = Math.min(heights[l],heights[r]);
+            int width =  r - l;
+            int product = width * length;
+            if(res < product) res = product;
+            if(heights[l] < heights[r]){
                 l++;
-               }else{
-                //match found
-                res.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                l++;
+            }else{
                 r--;
-                //dupe check
-                while(l < r && nums[l] == nums[l-1]){
-                    l++;
-                }
-               }
             }
-
-
         }
 
         return res;
