ed21939e0e7ba09563c0429dce423033a720b19c
Isac Pederson <isacjped@gmail.com>
2025-12-29 17:43:16 -0800

    Added accepted on 2025-12-30

--- 2025-12-30/accepted/solution.java
+++ 2025-12-30/accepted/solution.java
@@ -1,34 +1,19 @@
 class Solution {
-    public int evalRPN(String[] tokens) { 
-        Stack<Integer> stack = new Stack<>();
-        for(String token : tokens){
-            if(token.equals("+")){
-                int a = stack.pop();
-                int b = stack.pop();
-                stack.push(a+b);
-            }
-            else if(token.equals("-")){
-                int a = stack.pop();
-                int b = stack.pop();
-                stack.push(b-a);
-            }
-            else if(token.equals("/")){
-                int a = stack.pop();
-                int b = stack.pop();
-                stack.push(b/a);
-            }
-            else if(token.equals("*")){
-                int a = stack.pop();
-                int b = stack.pop();
-                stack.push(a*b);
-            }else{
-                stack.push(Integer.parseInt(token));
-            }
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int[] res = new int[temperatures.length];
+        Stack<int[]> stack = new Stack<>();
 
 
+        for(int i = 0; i < temperatures.length; i++){
+            int t = temperatures[i];
+            while(!stack.isEmpty() && t > stack.peek()[0]){
+                int[] pair = stack.pop();
+                res[pair[1]] = i - pair[1];
+            }
+            stack.push(new int[]{t,i});
         }
-
-        return stack.peek();
+        return res;
         
     }
 }
