3f90de1f50e7399d2537af94202b7b81da103f2c
Isac Pederson <isacjped@gmail.com>
2025-12-29 17:40:47 -0800

    Added accepted on 2025-12-30

--- 2025-12-30/accepted/solution.java
+++ 2025-12-30/accepted/solution.java
@@ -1,38 +1,23 @@
 class Solution {
-    public int longestConsecutive(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length()-1;
 
-        if(nums == null || nums.length == 0){
-            return 0;
-        }
-
-        for(int n : nums){
-            set.add(n);
-        }
-
-        int longest = 1;
-        int result = 1;
-
-        for(int number : set){
-            if(set.contains(number - 1)){
-                continue;
+        while(left < right){
+            while(!Character.isLetterOrDigit(s.charAt(right)) && left < right){
+                right--;
+            };
+            while(!Character.isLetterOrDigit(s.charAt(left)) && left < right){
+                left++;
+            };
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
             }
-            while(set.contains(number + 1)){
-                System.out.println(number);
-                longest++;
-                number = number+1;
-                System.out.println("NUMBER AFTER: " + number);
-            }
-            result = Math.max(longest,result);
-            longest = 1;
+            right--;
+            left++;
         }
 
-        return result;
-
-        
-
-
-
+        return true;
         
     }
 }
