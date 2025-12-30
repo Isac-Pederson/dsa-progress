4707c731bb306f32a85c8e7fca0b09e6af60b200
Isac Pederson <isacjped@gmail.com>
2025-12-29 17:40:30 -0800

    Added accepted on 2025-12-30

--- 2025-12-30/accepted/solution.java
+++ 2025-12-30/accepted/solution.java
@@ -1,30 +1,28 @@
 class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Map<Integer, Set<Character>> row = new HashMap<>();
+        Map<Integer, Set<Character>> col= new HashMap<>();
+        Map<String , Set<Character>> squares = new HashMap<>();
 
-    public int[] productExceptSelf(int[] nums) {
-        int[] suff = new int[nums.length];
-        int[] pref = new int[nums.length];
-        int[] res = new int[nums.length];
+            for(int r = 0; r < 9;r++){
+                for(int c = 0; c < 9;c++){ 
+                    if(board[r][c] == '.') continue;
 
-        pref[0] = 1;
-        suff[nums.length -1] = 1; 
+                    String squareKey = (r / 3) + "," + (c / 3);
 
-        for(int i = 1; i < nums.length; i++){
-            pref[i] = nums[i-1] * pref[i-1]; 
-        }
+                    if(row.computeIfAbsent(r,k -> new HashSet<>()).contains(board[r][c]) || 
+                        col.computeIfAbsent(c,k -> new HashSet<>()).contains(board[r][c]) || 
+                        squares.computeIfAbsent(squareKey,k -> new HashSet<>()).contains(board[r][c]) ){
+                        return false;
+                    }
 
-        for(int i = nums.length-2; i >= 0; i--){
-            suff[i] = nums[i+1] * suff[i+1]; 
-        }
+                    row.get(r).add(board[r][c]);
+                    col.get(c).add(board[r][c]);
+                    squares.get(squareKey).add(board[r][c]);
+                }
+            }
 
-        for(int i = 0; i < nums.length; i++){
-            res[i] = pref[i] * suff[i]; 
-        }
-
-
-
-        return res;
+        return true;
+        
     }
-
-
-
-}  
+}
